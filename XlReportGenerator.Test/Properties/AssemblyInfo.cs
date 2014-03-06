aa3c6f83713054c396c04125717b933df31bedaa
Yusak Setiawan <yoesak@yahoo.com>
2014-03-06 08:54:55 +0700

    Initial State

---
+++ XlReportGenerator.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("XlReportGenerator.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XlReportGenerator.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("573fe70b-aa43-4dec-8df1-d51d4cc2799f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ XlReportGenerator/XlReportGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XlReportGenerator
+{
+    public class XlReportGenerator
+    {
+        #region "Properties"
+        public String TempGeneratedFolder { get; private set; }
+        public String GeneratedFileName { get; private set; }
+        #endregion
+
+        #region "Constructor"
+        public XlReportGenerator(String tempGeneratedFolder)
+        {
+        }
+        #endregion
+
+        #region "Private Methods"
+        #endregion
+
+    }
+}
